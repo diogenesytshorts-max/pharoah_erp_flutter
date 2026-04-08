c6624b09e4bb7460fb0ea305d9f51d510b50203a
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-08 15:11:19 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'pharoah_manager.dart';
-import 'dashboard_view.dart'; // Nayi file import kari
+import 'dashboard_view.dart';
+import 'setup_view.dart';
+import 'login_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => PharoahManager()),
-      ],
+    ChangeNotifierProvider(
+      create: (_) => PharoahManager(),
       child: const MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isSetupDone = false;
+  bool isLoggedIn = false;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkStatus();
+  }
+
+  void _checkStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isSetupDone = prefs.getBool('isSetupDone') ?? false;
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) return const MaterialApp(home: Scaffold(body: Center(child: CircularProgressIndicator())));
+
     return MaterialApp(
       title: 'Pharoah ERP',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      // Ab seedha DashboardView dikhayega
-      home: const DashboardView(),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue), useMaterial3: true),
+      home: !isSetupDone 
+        ? SetupView(onComplete: () => setState(() => isSetupDone = true))
+        : (!isLoggedIn ? LoginView(onLogin: () => setState(() => isLoggedIn = true)) : const DashboardView()),
     );
   }
 }
