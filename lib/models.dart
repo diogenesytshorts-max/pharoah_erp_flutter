8fdb8e748a720452ca56697df542a65f31512bb8
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-08 21:21:52 +0530

    Update models.dart

--- lib/models.dart
+++ lib/models.dart
@@ -5,24 +5,24 @@ class BatchInfo {
   double mrp, rate;
   BatchInfo({required this.batch, required this.exp, required this.packing, required this.mrp, required this.rate});
   Map<String, dynamic> toMap() => {'batch': batch, 'exp': exp, 'packing': packing, 'mrp': mrp, 'rate': rate};
-  factory BatchInfo.fromMap(Map<String, dynamic> map) => BatchInfo(batch: map['batch'], exp: map['exp'], packing: map['packing'], mrp: (map['mrp']??0).toDouble(), rate: (map['rate']??0).toDouble());
+  factory BatchInfo.fromMap(Map<String, dynamic> map) => BatchInfo(batch: map['batch']??"", exp: map['exp']??"", packing: map['packing']??"", mrp: (map['mrp']??0).toDouble(), rate: (map['rate']??0).toDouble());
 }
 
 class Medicine {
   String id, name, packing, manufacturer, composition, hsnCode;
   double gst, mrp, rateA, rateB, rateC;
   int stock;
-   Medicine({required this.id, required this.name, required this.packing, this.manufacturer = "N/A", this.composition = "N/A", this.hsnCode = "N/A", this.gst = 12.0, required this.mrp, required this.rateA, required this.rateB, required this.rateC, this.stock = 0});
+  Medicine({required this.id, required this.name, required this.packing, this.manufacturer = "N/A", this.composition = "N/A", this.hsnCode = "N/A", this.gst = 12.0, required this.mrp, required this.rateA, required this.rateB, required this.rateC, this.stock = 0});
   Map<String, dynamic> toMap() => {'id': id, 'name': name, 'packing': packing, 'manufacturer': manufacturer, 'composition': composition, 'hsnCode': hsnCode, 'gst': gst, 'mrp': mrp, 'rateA': rateA, 'rateB': rateB, 'rateC': rateC, 'stock': stock};
-  factory Medicine.fromMap(Map<String, dynamic> map) => Medicine(id: map['id'], name: map['name'], packing: map['packing'], manufacturer: map['manufacturer'], composition: map['composition'], hsnCode: map['hsnCode'], gst: (map['gst']??12).toDouble(), mrp: (map['mrp']??0).toDouble(), rateA: (map['rateA']??0).toDouble(), rateB: (map['rateB']??0).toDouble(), rateC: (map['rateC']??0).toDouble(), stock: map['stock']??0);
+  factory Medicine.fromMap(Map<String, dynamic> map) => Medicine(id: map['id'], name: map['name'], packing: map['packing'], manufacturer: map['manufacturer']??"N/A", composition: map['composition']??"N/A", hsnCode: map['hsnCode']??"N/A", gst: (map['gst']??12).toDouble(), mrp: (map['mrp']??0).toDouble(), rateA: (map['rateA']??0).toDouble(), rateB: (map['rateB']??0).toDouble(), rateC: (map['rateC']??0).toDouble(), stock: map['stock']??0);
 }
 
 class Party {
   String id, name, address, city, route, phone, email, dl, gst, rateType;
-  double openingBalance;
-  Party({required this.id, required this.name, this.address = "", this.city = "", this.route = "", this.phone = "", this.email = "", this.dl = "N/A", this.gst = "N/A", this.rateType = "A", this.openingBalance = 0.0});
-  Map<String, dynamic> toMap() => {'id': id, 'name': name, 'address': address, 'city': city, 'route': route, 'phone': phone, 'email': email, 'dl': dl, 'gst': gst, 'rateType': rateType, 'openingBalance': openingBalance};
-  factory Party.fromMap(Map<String, dynamic> map) => Party(id: map['id'], name: map['name'], address: map['address'] ?? "", city: map['city'] ?? "", route: map['route'] ?? "", phone: map['phone'] ?? "", email: map['email'] ?? "", dl: map['dl'] ?? "N/A", gst: map['gst'] ?? "N/A", rateType: map['rateType'] ?? "A", openingBalance: (map['openingBalance'] ?? 0.0).toDouble());
+  double openingBalance, specialDiscount; // ADDED specialDiscount
+  Party({required this.id, required this.name, this.address = "", this.city = "", this.route = "", this.phone = "", this.email = "", this.dl = "N/A", this.gst = "N/A", this.rateType = "A", this.openingBalance = 0.0, this.specialDiscount = 0.0});
+  Map<String, dynamic> toMap() => {'id': id, 'name': name, 'address': address, 'city': city, 'route': route, 'phone': phone, 'email': email, 'dl': dl, 'gst': gst, 'rateType': rateType, 'openingBalance': openingBalance, 'specialDiscount': specialDiscount};
+  factory Party.fromMap(Map<String, dynamic> map) => Party(id: map['id'], name: map['name'], address: map['address']??"", city: map['city']??"", route: map['route']??"", phone: map['phone']??"", email: map['email']??"", dl: map['dl']??"N/A", gst: map['gst']??"N/A", rateType: map['rateType']??"A", openingBalance: (map['openingBalance']??0.0).toDouble(), specialDiscount: (map['specialDiscount']??0.0).toDouble());
 }
 
 class BillItem {
