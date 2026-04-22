c63ad765fd34aad7d062f865603ede7b80158917
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-23 00:32:14 +0530

    Update item_ledger_view.dart

--- lib/item_ledger_view.dart
+++ lib/item_ledger_view.dart
@@ -1,3 +1,5 @@
+import 'bill_view_only.dart';
+import 'purchase/purchase_view_only.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
