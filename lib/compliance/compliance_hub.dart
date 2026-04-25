0ac125f4f5fbbc18bb86ff9420426b8d3604b850
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-25 22:59:05 +0530

    Create compliance_hub.dart

---
+++ lib/compliance/compliance_hub.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+class ComplianceHub extends StatelessWidget {
+  const ComplianceHub({super.key});
+  @override build(context) => Scaffold(appBar: AppBar(title: const Text("Compliance Hub"), backgroundColor: Colors.teal.shade800));
+}
