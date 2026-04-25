7bd97e6be5290e1edfdad8a5ed2b2c79c483fea9
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-25 22:58:27 +0530

    Create staff_management_view.dart

---
+++ lib/administration/staff_management_view.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+class StaffManagementView extends StatelessWidget {
+  const StaffManagementView({super.key});
+  @override build(context) => Scaffold(appBar: AppBar(title: const Text("Security & Staff"), backgroundColor: Colors.red.shade800));
+}
