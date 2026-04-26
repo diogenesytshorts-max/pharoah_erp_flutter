6721f191f28dd718a6753fc3c7c6d8f999662d2e
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-27 01:13:40 +0530

    Update demo_data.dart

--- lib/demo_data.dart
+++ lib/demo_data.dart
@@ -1,14 +1,16 @@
+// FILE: lib/demo_data.dart (Replacement Code)
+
 import 'models.dart';
 
 class DemoData {
   static List<Medicine> getMedicines() {
-    // Demo products mapped to the new Universal ID Series
     return [
       Medicine(
         id: "demo_1",
-        systemId: "PH-10001",
+        systemId: "PH-D-10001", // 'D' for Demo to avoid overlap
         name: "DOLO 650 MG",
         packing: "15 TAB",
+        companyId: "MICRO LABS",
         mrp: 30.91,
         purRate: 25.40,
         rateA: 28.50,
@@ -17,12 +19,15 @@ class DemoData {
         stock: 0.0,
         gst: 12.0,
         hsnCode: "3004",
+        isScheduleH1: false,
+        isNarcotic: false,
       ),
       Medicine(
         id: "demo_2",
-        systemId: "PH-10002",
+        systemId: "PH-D-10002",
         name: "PAN 40 MG",
         packing: "10 TAB",
+        companyId: "ALKEM LABORATORIES",
         mrp: 120.00,
         purRate: 95.00,
         rateA: 110.00,
@@ -34,42 +39,47 @@ class DemoData {
       ),
       Medicine(
         id: "demo_3",
-        systemId: "PH-10003",
-        name: "CALPOL 500",
-        packing: "15 TAB",
+        systemId: "PH-D-10003",
+        name: "AZITHRAL 500",
+        packing: "5 TAB",
+        companyId: "ALEMBIC PHARMA",
+        mrp: 115.00,
+        purRate: 88.00,
+        rateA: 105.00,
+        rateB: 100.00,
+        rateC: 98.00,
+        stock: 0.0,
+        gst: 12.0,
+        hsnCode: "3004",
+        isScheduleH1: true, // Test ke liye H1 On kiya hai
+      ),
+      Medicine(
+        id: "demo_4",
+        systemId: "PH-D-10004",
+        name: "FORTWIN INJECTION",
+        packing: "1 ML",
+        companyId: "SANOFI INDIA",
         mrp: 15.50,
-        purRate: 12.00,
+        purRate: 10.00,
         rateA: 14.50,
         rateB: 14.00,
         rateC: 13.50,
         stock: 0.0,
         gst: 12.0,
         hsnCode: "3004",
+        isNarcotic: true, // Test ke liye Narcotic On kiya hai
       ),
       Medicine(
-        id: "demo_4",
-        systemId: "PH-10004",
+        id: "demo_5",
+        systemId: "PH-D-10005",
         name: "LIMCEE 500",
         packing: "15 TAB",
+        companyId: "ABBOTT INDIA",
         mrp: 25.00,
         purRate: 18.00,
         rateA: 23.00,
         rateB: 22.00,
         rateC: 20.00,
-        stock: 0.0,
-        gst: 12.0,
-        hsnCode: "3004",
-      ),
-      Medicine(
-        id: "demo_5",
-        systemId: "PH-10005",
-        name: "AZITHRAL 500",
-        packing: "5 TAB",
-        mrp: 115.00,
-        purRate: 88.00,
-        rateA: 105.00,
-        rateB: 100.00,
-        rateC: 98.00,
         stock: 0.0,
         gst: 12.0,
         hsnCode: "3004",
