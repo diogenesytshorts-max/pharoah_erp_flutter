d1c3d1f0b6b1e929ccc76290625f53727e088371
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-27 00:37:15 +0530

    Update system_user_model.dart

--- lib/administration/system_user_model.dart
+++ lib/administration/system_user_model.dart
@@ -1,4 +1,4 @@
-// FILE: lib/administration/system_user_model.dart (Replace Full)
+// FILE: lib/administration/system_user_model.dart
 
 import 'dart:convert';
 
@@ -7,37 +7,52 @@ class SystemUser {
   
   // --- PERMISSIONS ---
   bool canDeleteBill;
-  bool canEditBill;         // NAYA
+  bool canEditBill;         
   bool canViewPurchaseRate; 
   bool canViewFinance;      
-  bool canExportData;       
+  bool canExportData;  
+  bool canRunMaintenance;   // NAYA: Permission for File Maintenance
 
   SystemUser({
-    required this.id, required this.name, required this.username, required this.password,
+    required this.id, 
+    required this.name, 
+    required this.username, 
+    required this.password,
     this.canDeleteBill = false,
-    this.canEditBill = false,         // DEFAULT OFF
+    this.canEditBill = false,         
     this.canViewPurchaseRate = false,
     this.canViewFinance = false,
     this.canExportData = false,
+    this.canRunMaintenance = false, // Default OFF rahega
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id, 'name': name, 'username': username, 'password': password,
-      'canDeleteBill': canDeleteBill, 'canEditBill': canEditBill,
-      'canViewPurchaseRate': canViewPurchaseRate, 'canViewFinance': canViewFinance,
+      'id': id, 
+      'name': name, 
+      'username': username, 
+      'password': password,
+      'canDeleteBill': canDeleteBill, 
+      'canEditBill': canEditBill,
+      'canViewPurchaseRate': canViewPurchaseRate, 
+      'canViewFinance': canViewFinance,
       'canExportData': canExportData,
+      'canRunMaintenance': canRunMaintenance, // JSON mein save karne ke liye
     };
   }
 
   factory SystemUser.fromMap(Map<String, dynamic> map) {
     return SystemUser(
-      id: map['id'] ?? '', name: map['name'] ?? '', username: map['username'] ?? '', password: map['password'] ?? '',
+      id: map['id'] ?? '', 
+      name: map['name'] ?? '', 
+      username: map['username'] ?? '', 
+      password: map['password'] ?? '',
       canDeleteBill: map['canDeleteBill'] ?? false,
       canEditBill: map['canEditBill'] ?? false,
       canViewPurchaseRate: map['canViewPurchaseRate'] ?? false,
       canViewFinance: map['canViewFinance'] ?? false,
       canExportData: map['canExportData'] ?? false,
+      canRunMaintenance: map['canRunMaintenance'] ?? false, // Load karte waqt safety (?? false)
     );
   }
 }
