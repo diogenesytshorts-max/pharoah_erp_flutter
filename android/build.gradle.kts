747ccc63cd5b1df2bdef7e7cfe717dea6a7e0a9a
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-29 01:32:58 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,31 +3,13 @@ allprojects {
         google()
         mavenCentral()
     }
-    
-    // FORCE ALL PLUGINS TO USE STABLE VERSIONS
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "androidx.core" && requested.name == "core") {
-                useVersion("1.13.1")
-            }
-            if (requested.group == "androidx.core" && requested.name == "core-ktx") {
-                useVersion("1.13.1")
-            }
-            if (requested.group == "androidx.activity") {
-                useVersion("1.9.3")
-            }
-            if (requested.group == "androidx.lifecycle") {
-                useVersion("2.8.7")
-            }
-        }
-    }
 }
 
-val newBuildDir = rootProject.layout.projectDirectory.dir("../../build")
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
