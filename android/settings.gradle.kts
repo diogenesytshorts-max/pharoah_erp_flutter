90328665ee63216b436b96c1175c23a1661a8cad
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-28 22:24:11 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,8 +20,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.3.2" apply false // <-- Yahan 8.1.0 ko 8.3.2 kiya
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false // Kotlin bhi update kar di safety ke liye
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
