ffb4d3916c1550effbf447f541a564381b2c8e7f
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-29 00:17:29 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.rawat.pharoah_erp"
-    // UPDATED: Required by latest AndroidX activity libraries
+    // Required by latest AndroidX activity libraries
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,6 @@ android {
     defaultConfig {
         applicationId = "com.rawat.pharoah_erp"
         minSdk = 24 
-        // UPDATED: Aligned with compileSdk
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -36,13 +35,16 @@ android {
     }
 }
 
-// THE CURE FOR lStar ERROR:
-// Forcing 1.15.0-alpha01 or higher is needed for SDK 36 compatibility
+// THE CURE FOR lStar ERROR & ACTIVITY DEPENDENCY CRASH:
 configurations.all {
     resolutionStrategy {
         force("androidx.core:core:1.15.0-alpha01")
         force("androidx.core:core-ktx:1.15.0-alpha01")
         force("androidx.annotation:annotation:1.9.1")
+        
+        // AndroidX Activity crash fix
+        force("androidx.activity:activity:1.9.3")
+        force("androidx.activity:activity-ktx:1.9.3")
     }
 }
 
