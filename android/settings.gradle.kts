cec95b06f238dcffd4da07e69573dfddad42ce9c
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-04-29 01:01:43 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
             propertiesFile.inputStream().use { properties.load(it) }
         }
         val sdkPath = properties.getProperty("flutter.sdk")
-        requireNotNull(sdkPath) { "Flutter SDK not found. Define 'flutter.sdk' in local.properties or environment." }
+        requireNotNull(sdkPath) { "Flutter SDK not found." }
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -20,7 +20,6 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    // इसे वापस 8.7.3 पर सेट किया गया है
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
