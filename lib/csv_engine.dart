535218441578ebcf82dc0699ae46089fd90d3cc2
diogenesytshorts-max <diogenesytshorts@gmail.com>
2026-05-16 22:06:51 +0530

    Update csv_engine.dart

--- lib/csv_engine.dart
+++ lib/csv_engine.dart
@@ -6,13 +6,14 @@ import 'gateway/company_registry_model.dart';
 
 class CsvEngine {
   // 37 Columns Header
+  // --- UNIVERSAL 38-COLUMN HEADER (Index 0 to 37) ---
   static List<String> get _universalHeader => [
     "DATE", "BILL_NO", // 0, 1
     "PARTY_NAME", "PARTY_GST", "PARTY_DL", "PARTY_PAN", "PARTY_MOBILE", "PARTY_EMAIL", "PARTY_ADDRESS", "PARTY_CITY", "PARTY_STATE", // 2-10
     "SENDER_NAME", "SENDER_GST", "SENDER_DL", "SENDER_PAN", "SENDER_MOBILE", "SENDER_EMAIL", "SENDER_ADDRESS", // 11-17
     "ITEM_NAME", "ITEM_PACKING", "ITEM_HSN", "ITEM_MFG", "ITEM_SALT", "ITEM_FORM", "IS_NARCOTIC", "IS_H1", // 18-25
     "ITEM_BATCH", "ITEM_EXP", "QTY", "FREE", "MRP", "PUR_RATE", "SALE_RATE", "GST_PER", // 26-33
-    "ITEM_TOTAL", "ITEM_DISCOUNT_PER", "BILL_ROUNDOFF" // 34, 35, 36
+    "ITEM_TOTAL", "ITEM_DISCOUNT_PER", "ITEM_DISCOUNT_AMT", "BILL_ROUNDOFF" // 34, 35, 36, 37
   ];
 
   static String convertSalesToCsv({
@@ -37,12 +38,12 @@ class CsvEngine {
         }
 
         rows.add([
-          DateFormat('dd/MM/yyyy').format(s.date), s.billNo,
-          s.partyName, s.partyGstin, s.partyDl, s.partyPan, finalMobile, s.partyEmail, s.partyAddress, s.partyCity, s.partyState,
-          shop.name, shop.gstin, shop.dlNo, "N/A", shop.phone, shop.email, shop.address,
-          i.name, i.packing, i.hsn, mfg, salt, med.drugForm, med.isNarcotic ? "YES" : "NO", med.isScheduleH1 ? "YES" : "NO",
-          i.batch, i.exp, i.qty, i.freeQty, i.mrp, maskPurchaseRate ? 0.0 : med.purRate, i.rate, i.gstRate,
-          i.total, discPer, s.roundOff // Index 35 par ab asli Discount % jayega
+          DateFormat('dd/MM/yyyy').format(s.date), s.billNo, // 0, 1
+          s.partyName, s.partyGstin, s.partyDl, s.partyPan, finalMobile, s.partyEmail, s.partyAddress, s.partyCity, s.partyState, // 2-10
+          shop.name, shop.gstin, shop.dlNo, "N/A", shop.phone, shop.email, shop.address, // 11-17
+          i.name, i.packing, i.hsn, mfg, salt, med.drugForm, med.isNarcotic ? "YES" : "NO", med.isScheduleH1 ? "YES" : "NO", // 18-25
+          i.batch, i.exp, i.qty, i.freeQty, i.mrp, maskPurchaseRate ? 0.0 : med.purRate, i.rate, i.gstRate, // 26-33
+          i.total, discPer, i.discountRupees, s.roundOff // 34, 35, 36 (AMT), 37
         ]);
       }
     }
@@ -61,11 +62,12 @@ class CsvEngine {
         String mfg = allComps.firstWhere((c) => c.id == med.companyId, orElse: () => Company(id: '', name: 'N/A')).name;
         String salt = allSalts.firstWhere((sl) => sl.id == med.saltId, orElse: () => Salt(id: '', name: 'N/A')).name;
         rows.add([
-          DateFormat('dd/MM/yyyy').format(p.date), p.billNo,
-          p.distributorName, masterParty.gst, masterParty.dl, masterParty.pan, masterParty.phone, masterParty.email, masterParty.address, masterParty.city, masterParty.state,
-          shop.name, shop.gstin, shop.dlNo, "N/A", shop.phone, shop.email, shop.address,
-          i.name, i.packing, i.hsn, mfg, salt, med.drugForm, med.isNarcotic ? "YES" : "NO", med.isScheduleH1 ? "YES" : "NO",
-          i.batch, i.exp, i.qty, i.freeQty, i.mrp, i.purchaseRate, i.rateA, i.gstRate, i.total, 0.0, 0.0
+          DateFormat('dd/MM/yyyy').format(p.date), p.billNo, // 0, 1
+          p.distributorName, masterParty.gst, masterParty.dl, masterParty.pan, masterParty.phone, masterParty.email, masterParty.address, masterParty.city, masterParty.state, // 2-10
+          shop.name, shop.gstin, shop.dlNo, "N/A", shop.phone, shop.email, shop.address, // 11-17
+          i.name, i.packing, i.hsn, mfg, salt, med.drugForm, med.isNarcotic ? "YES" : "NO", med.isScheduleH1 ? "YES" : "NO", // 18-25
+          i.batch, i.exp, i.qty, i.freeQty, i.mrp, i.purchaseRate, i.rateA, i.gstRate, // 26-33
+          i.total, i.discountPer, i.discountRupees, 0.0 // 34, 35, 36 (AMT), 37
         ]);
       }
     }
